ce1a1cca43f393e20d1f8815c5bf6cf6421347c2
Martin van Zyl <martin.vanzyl45@gmail.com>
2020-03-16 21:17:02 +0200

    Foo-Bar

---
+++ src/main/java/guru/springframework/spring5webapp/Foo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guru.springframework.spring5webapp;
+
+/**
+ *
+ * @author koos
+ */
+public class Foo
+{
+    public Bar bar()
+    {
+        return new Bar();
+    }
+    
+}
