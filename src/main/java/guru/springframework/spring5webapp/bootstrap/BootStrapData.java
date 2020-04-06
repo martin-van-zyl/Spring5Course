f1d82c4ee7fa3ff730edec294e4eb01c232ddc3a
Martin van Zyl <martin.vanzyl45@gmail.com>
2020-04-06 14:11:45 +0200

    Time for a merge

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -7,8 +7,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -22,37 +24,59 @@ public class BootStrapData implements CommandLineRunner
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository)
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository)
     {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
-    
-    
 
     @Override
     public void run(String... args) throws Exception
     {
+        Publisher mb = new Publisher("Mopani Books", "185 Steve Biko Rd", "Bela Bela", "Polokwane", "9870");
+        publisherRepository.save(mb);
+        
         Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain Driven Design", "123123");
+        Book ddd = new Book("Domain Driven Design", "123123", mb);
+
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
+
+        mb.getBooks().add(ddd);
+
         
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(mb); //Entity Changed - so save again
+
         
+        Publisher juta = new Publisher("Juta", "462 Malibongwe Drive", "Roodepoort", "Gauteng", "1234");
         
+        publisherRepository.save(juta);
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "393459459");
+        Book noEJB = new Book("J2EE Development without EJB", "393459459", juta);
+        
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
         
+        juta.getBooks().add(noEJB);
+
+        
         authorRepository.save(rod);
         bookRepository.save(noEJB);
-        
+        publisherRepository.save(juta); //Entity Changed - so save again
+
         System.out.println("Started in Bootstrap");
+        System.out.println("Number of Publishers: " + publisherRepository.count());
         System.out.println("Number of Books: " + bookRepository.count());
+        System.out.println("Number of Authors: " + authorRepository.count());
+        
+        System.out.println("Mopani Books Number of Books: " + mb.getBooks().size());
+        System.out.println("Juta Number of Books: " + juta.getBooks().size());
+
     }
 
 }
--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 
 /**
  *
@@ -30,6 +31,9 @@ public class Book
     private String title;
     private String isbn;
     
+    @ManyToOne
+    private Publisher publisher;
+    
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
@@ -38,10 +42,11 @@ public class Book
     {
     }
 
-    public Book(String title, String isbn)
+    public Book(String title, String isbn, Publisher publisher)
     {
         this.title = title;
         this.isbn = isbn;
+        this.publisher = publisher;
     }
 
     public Long getId()
@@ -74,6 +79,18 @@ public class Book
     {
         this.isbn = isbn;
     }
+
+    public Publisher getPublisher()
+    {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher)
+    {
+        this.publisher = publisher;
+    }
+    
+    
 
     public Set<Author> getAuthors()
     {
